be4d33cfb81a9ccd61d3d56093c15489dc8d2caa
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-06-01 23:40:11 +0530

    Time: 22 ms (89.87%) | Memory: 77.8 MB (84.13%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,53 +1,63 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         //Optimal Solution
-        int n=nums.length;
-        if(n==0) return 0;
-        int longest=1; 
-        HashSet<Integer> st=new HashSet<>();
-        for(int i=0; i<n; i++){
-            st.add(nums[i]);
-        }
-        for(int it:st){
-            if(!st.contains(it-1)){
-                int count=1;
-                int x=it;
-                while(st.contains(x+1)){
-                    count++;
-                    x+=1;
-                }
-                longest=Math.max(count,longest);
-
-
-            }
-        }
-        return longest;
-
-
-
-
-
-
-
-
-
-        // //Better solution
         // int n=nums.length;
-        // int lastSmallest=Integer.MIN_VALUE;
-        // int count=0;
-        // int longest=1;
-        // Arrays.sort(nums);
+        // if(n==0) return 0;
+        // int longest=1; 
+        // HashSet<Integer> st=new HashSet<>();
         // for(int i=0; i<n; i++){
-        //     if(nums[i]-1==lastSmallest){
-        //         count++;
-        //         lastSmallest=nums[i];
-        //     }else if(nums[i]!=lastSmallest){
+        //     st.add(nums[i]);
+        // }
+        // for(int it:st){
+        //     if(!st.contains(it-1)){
+        //         int count=1;
+        //         int x=it;
+        //         while(st.contains(x+1)){
+        //             count++;
+        //             x+=1;
+        //         }
+        //         longest=Math.max(count,longest);
+
+
+        //     }
+        // }
+        // return longest;
+
+
+
+
+
+
+
+
+
+        // Better solution
+        int n=nums.length;
+        if(n==0)return 0;
+        int lastSmallest=Integer.MIN_VALUE;
+        int count=0;
+        int longest=1;
+        Arrays.sort(nums);
+        for(int i=0; i<n; i++){
+            if(nums[i]-1==lastSmallest){
+                count++;
+                lastSmallest=nums[i];
+            }else if(nums[i]==lastSmallest){
+                continue;
+                //   count=1;
+                //   lastSmallest=nums[i];
+               }
+               else{
+                 count=1;
+                  lastSmallest=nums[i];
+               }
+        // else if(nums[i]!=lastSmallest){
         //         count=1;
         //         lastSmallest=nums[i];
         //     }
-        //     longest=Math.max(longest,count);
-        // }
-        // return longest;
+            longest=Math.max(longest,count);
+        }
+        return longest;
 
       //--> Brute force    
     // public boolean linearSearch(int x, int nums[]){
