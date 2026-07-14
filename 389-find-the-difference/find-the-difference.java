7d812b96699ee220f3f5c7830c59c47c31969024
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-15 04:33:14 +0530

    Time: 3 ms (41.25%) | Memory: 43.1 MB (71.78%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,19 +1,45 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character, Integer> mp=new HashMap<>();
+        //Approach 2
+        int sum1=0;
         for(int i=0; i<s.length(); i++){
-            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+            sum1+=s.charAt(i);
         }
+        int sum2=0;
         for(int i=0; i<t.length(); i++){
-            char c=t.charAt(i);
-            mp.put(c,mp.getOrDefault(c,0)-1);
-
-                if(mp.get(c)<0){
-                    return c;
-                }
+            sum2+=t.charAt(i);
         }
-        //flow of execution will never reach here;
-        return 'x';
+        return (char)(sum2-sum1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Approach 1
+        // HashMap<Character, Integer> mp=new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //     mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+        // }
+        // for(int i=0; i<t.length(); i++){
+        //     char c=t.charAt(i);
+        //     mp.put(c,mp.getOrDefault(c,0)-1);
+
+        //         if(mp.get(c)<0){
+        //             return c;
+        //         }
+        // }
+        // //flow of execution will never reach here;
+        // return 'x';
         
     }
 }
