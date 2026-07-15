17db93dde872d62b03f7578ad37236cfd30b30c1
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-15 16:05:23 +0530

    Time: 1 ms (17.56%) | Memory: 48.1 MB (71.19%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,71 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int ans1=-1;
-        int ans2=-1;
-        int low=0; 
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                ans1=mid;
-                high=mid-1;
-            }else{
-                if(nums[mid]>target){
-                    high=mid-1;
-                }else{
-                   low=mid+1;
+        int n=nums.length; 
+        int low=0;
+        int high=n-1;
+        int first=-1;
+        int last=-1;
+        for(int i=0; i<n; i++){
+            if(nums[i]==target){
+                if(first==-1){
+                    first=i;
                 }
+                last=i;
             }
         }
-        low=0;
-        high=nums.length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                ans2=mid;
-                low=mid+1;
-            }else{
-                if(nums[mid]>target){
-                    high=mid-1;
-                }else{
-                   low=mid+1;
-                }
-            }
-        }
-        return new int[]{ans1,ans2};
+        return new int[]{first,last};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Approach using Binary Search
+        // int ans1=-1;
+        // int ans2=-1;
+        // int low=0; 
+        // int high=nums.length-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]==target){
+        //         ans1=mid;
+        //         high=mid-1;
+        //     }else{
+        //         if(nums[mid]>target){
+        //             high=mid-1;
+        //         }else{
+        //            low=mid+1;
+        //         }
+        //     }
+        // }
+        // low=0;
+        // high=nums.length-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]==target){
+        //         ans2=mid;
+        //         low=mid+1;
+        //     }else{
+        //         if(nums[mid]>target){
+        //             high=mid-1;
+        //         }else{
+        //            low=mid+1;
+        //         }
+        //     }
+        // }
+        // return new int[]{ans1,ans2};
         
     }
 }
