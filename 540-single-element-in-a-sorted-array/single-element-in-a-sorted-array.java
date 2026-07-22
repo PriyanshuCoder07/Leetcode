0d1ef80e095695d5103867fd797d6f2b18a16101
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-22 20:00:53 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (77.88%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,23 +1,37 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n=nums.length;
-        // //Edge case
-        int xor=0;
-        for(int i=0; i<n; i++){
-            xor=xor^nums[i];
-        }
-        return xor;
-        // if(nums[0]!=nums[1]) return nums[0];
-        // if(nums[n-1]!=nums[n-2])return nums[n-1];
-        // int low=1;
-        // int high=n-2;
-        // while(low<high){
-        //     int mid=(low+high)/2;
-        //     if(nums[mid]!=nums[mid-1] || nums[mid]!=nums[mid+1]) return nums[mid];
-        //     if(n%2==1){
+            //Edge case
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        int low=1;
+        int high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
                 
-        //     }
+              //1,1,2,4,4,5,5,
+             // 0 1 2 3 4 5 6 
+             // e o e o e o e 
+             //e,o
+             //o,e
+            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1])){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+             
+        }
+        return -1;
+
+        // int n=nums.length;
+        // int xor=0;
+        // for(int i=0; i<n; i++){
+        //     xor=xor^nums[i];
         // }
+        // return xor;
+
         // if(n==1) return nums[0];
         // for(int i=0; i<nums.length; i++){
         //     if(i==0){
