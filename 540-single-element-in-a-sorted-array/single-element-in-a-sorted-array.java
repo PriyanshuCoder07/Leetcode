405af0c1bcfe4d38cc27e10867e7b47bfac42cac
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-22 18:16:04 +0530

    Time: 1 ms (24.84%) | Memory: 52.7 MB (77.88%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,18 +1,34 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n=nums.length;
-        //Edge case
-        if(n==1) return nums[0];
-        for(int i=0; i<nums.length; i++){
-            if(i==0){
-                if(nums[i]!=nums[i+1]) return nums[i];
-            }else if(i==n-1){
-                if(nums[i-1]!=nums[i]) return nums[i];
-            }else{
-                //iss condn me sare middle ke ele lie karenge
-                if(nums[i-1]!=nums[i] && nums[i]!=nums[i+1]) return nums[i];
-            }
+        // //Edge case
+        int xor=0;
+        for(int i=0; i<n; i++){
+            xor=xor^nums[i];
         }
-        return -1;
+        return xor;
+        // if(nums[0]!=nums[1]) return nums[0];
+        // if(nums[n-1]!=nums[n-2])return nums[n-1];
+        // int low=1;
+        // int high=n-2;
+        // while(low<high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]!=nums[mid-1] || nums[mid]!=nums[mid+1]) return nums[mid];
+        //     if(n%2==1){
+                
+        //     }
+        // }
+        // if(n==1) return nums[0];
+        // for(int i=0; i<nums.length; i++){
+        //     if(i==0){
+        //         if(nums[i]!=nums[i+1]) return nums[i];
+        //     }else if(i==n-1){
+        //         if(nums[i-1]!=nums[i]) return nums[i];
+        //     }else{
+        //         //iss condn me sare middle ke ele lie karenge
+        //         if(nums[i-1]!=nums[i] && nums[i]!=nums[i+1]) return nums[i];
+        //     }
+        // }
+        // return -1;
     }
 }
