da549aa84bd772d83c7da24c81546e38d83858b0
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-23 08:30:32 +0530

    Time: 1 ms (97.43%) | Memory: 42.2 MB (95.66%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int mySqrt(int x) {
+        //Binary Search 
+        long low=1;
+        long high=x; 
+        long ans=0;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return (int)ans;
+
+        //linear search having a tc of O(N)
+        // int ans=1;
+        // for(int i=1; i<=x; i++){
+        //     if(i*i<=x){
+        //         ans=i;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // return ans;
+        // return(int) Math.sqrt(x);
+        
+    }
+}
