c793fe6ad67a54fe21f0e087e9478d1ea09dacb6
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-24 16:26:53 +0530

    Time: 1 ms (97.44%) | Memory: 42.4 MB (79.16%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -24,9 +24,11 @@ class Solution {
         long ans=0;
         while(low<=high){
             long mid=low+(high-low)/2;
-            if(mid*mid<=x){
+            if(mid*mid==x){
+                return (int)mid;
+            }else if(mid*mid<x){
                 ans=mid;
-                low=mid+1;
+                low=mid+1; 
             }else{
                 high=mid-1;
             }
