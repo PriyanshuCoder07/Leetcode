73c051606a017b2443f9b7fa1a7e177ffd790032
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-24 16:25:01 +0530

    Time: 1 ms (97.44%) | Memory: 42.9 MB (8.30%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,13 +1,30 @@
 class Solution {
     public int mySqrt(int x) {
-        //Approach using binary search
+        // int ans=1;
+        // for(long i=0; i<=x; i++){
+        //     if(i*i==x){
+        //         ans=(int)i;
+        //         return ans;
+        //     }else if(i*i<=x){
+        //         ans=(int)i;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // return ans;
+
+
+
+
+
+
+        // //Binary Search 
         long low=0;
         long high=x; 
         long ans=0;
         while(low<=high){
             long mid=low+(high-low)/2;
-            if(mid*mid==x)return (int)mid;
-            else if(mid*mid<x){
+            if(mid*mid<=x){
                 ans=mid;
                 low=mid+1;
             }else{
@@ -16,48 +33,16 @@ class Solution {
         }
         return (int)ans;
 
-
-
-
-
-
-
-
-
-
-        // //Approach using linear search
-        // long ans=1;
-        // for(long i=0; i<=x; i++){
-        //     if(i*i==x){
-        //         return (int)i;
-        //     }else if(i*i<x){
+        //linear search having a tc of O(N)
+        // int ans=1;
+        // for(int i=1; i<=x; i++){
+        //     if(i*i<=x){
         //         ans=i;
         //     }else{
         //         break;
         //     }
         // }
-        // return (int)ans;
-
-
-
-
-
-
-        // //Binary Search 
-        // long low=1;
-        // long high=x; 
-        // long ans=0;
-        // while(low<=high){
-        //     long mid=low+(high-low)/2;
-        //     if(mid*mid<=x){
-        //         ans=mid;
-        //         low=mid+1;
-        //     }else{
-        //         high=mid-1;
-        //     }
-        // }
-        // return (int)ans;
-
+        // return ans;
         // return(int) Math.sqrt(x);
         
     }
