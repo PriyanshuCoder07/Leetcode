0e4a56f85257247545c8bd10816b23acb3fa71cb
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-07-25 16:17:33 +0530

    Time: 12 ms (51.87%) | Memory: 48 MB (33.57%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,33 +1,67 @@
 class Solution {
-    public long solve(int[] piles,int speed){
-        int n=piles.length;
-        long eatspeed=0;
-        for(int i=0; i<n; i++){
-            eatspeed+=piles[i]/speed;
-            if(piles[i]%speed!=0) eatspeed++;
+    public boolean isPossible(int[] piles, int n, int h){
+        long ans=0;
+        for(int i=0; i<piles.length; i++){
+            if(piles[i]<=n){
+                ans++;
+            }else{
+                if(piles[i]>n && piles[i]%n==0){
+                    ans+=piles[i]/n;
+                }else{
+                    ans+=(piles[i]/n)+1;
+                }
+            }
         }
-        return eatspeed;
+        return ans<=h;
     }
     public int minEatingSpeed(int[] piles, int h) {
         int max=0;
         for(int i=0; i<piles.length; i++){
-            max=Math.max(piles[i],max);
+            max=Math.max(max,piles[i]);
         }
         int low=1;
         int high=max;
-        int res=0;
+        int eatingspeed=1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            long hrs=solve(piles,mid);
-            if(hrs>h){
-                low=mid+1;
-            }else{
-                res=mid;
+            if(isPossible(piles,mid,h)){
+                eatingspeed=mid;
                 high=mid-1;
+            }else{
+                low=mid+1;
             }
-
         }
-        return res;
+        return eatingspeed;
+        
+    // public long solve(int[] piles,int speed){
+    //     int n=piles.length;
+    //     long eatspeed=0;
+    //     for(int i=0; i<n; i++){
+    //         eatspeed+=piles[i]/speed;
+    //         if(piles[i]%speed!=0) eatspeed++;
+    //     }
+    //     return eatspeed;
+    // }
+    // public int minEatingSpeed(int[] piles, int h) {
+    //     int max=0;
+    //     for(int i=0; i<piles.length; i++){
+    //         max=Math.max(piles[i],max);
+    //     }
+    //     int low=1;
+    //     int high=max;
+    //     int res=0;
+    //     while(low<=high){
+    //         int mid=low+(high-low)/2;
+    //         long hrs=solve(piles,mid);
+    //         if(hrs>h){
+    //             low=mid+1;
+    //         }else{
+    //             res=mid;
+    //             high=mid-1;
+    //         }
+
+    //     }
+    //     return res;
       
         
     }
