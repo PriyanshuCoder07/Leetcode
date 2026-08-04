7c309e12f36a5d82eaf8096b7b0a3ea0e53360d0
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-08-04 21:42:20 +0530

    Time: 3 ms (81.76%) | Memory: 47.1 MB (33.52%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -2,16 +2,55 @@ class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         List<Integer> ans=new ArrayList<>();
         HashSet<Integer> st=new HashSet<>();
+        int left=Integer.MAX_VALUE;
+        int right=0;
         for(int i=0; i<nums.length; i++){
+            left=Math.min(nums[i],left);
+            right=Math.max(nums[i],right);
             st.add(nums[i]);
         }
-        Arrays.sort(nums);
-        for(int i=nums[0]; i<nums[nums.length-1]; i++){
-            while(!st.contains(i)){
-                ans.add(i);
-                i++;
+       
+        // int val=left;
+        while(left<right){
+            if(st.contains(left)){
+            }else{
+                ans.add(left);
             }
+            left++;
+            // right--;
         }
         return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // HashSet<Integer> st=new HashSet<>();
+        // for(int i=0; i<nums.length; i++){
+        //     st.add(nums[i]);
+        // }
+        // Arrays.sort(nums);
+        // for(int i=nums[0]; i<nums[nums.length-1]; i++){
+        //     while(!st.contains(i)){
+        //         ans.add(i);
+        //         i++;
+        //     }
+        // }
+        // return ans;
     }
 }
