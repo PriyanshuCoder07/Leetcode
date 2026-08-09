cbe3cb4059af27e8ffe8e77531fd67b539e2faff
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-08-09 18:31:24 +0530

    Time: 2 ms (47.24%) | Memory: 48.9 MB (33.06%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,20 +3,65 @@ class Solution {
         int n=nums1.length; 
         int m=nums2.length;
         int x=n+m;
-        int[] nums3=new int[x];
+        int nums3[]=new int[x];
+        //using two pointer
+        int i=0;
+        int j=0;
         int ptr=0;
-        for(int i=0; i<n; i++){
-            nums3[ptr]=nums1[i];
-            ptr++;
+        while(i<n && j<m){
+            if(nums1[i]<=nums2[j]){
+                nums3[ptr]=nums1[i];
+                i++;
+                ptr++;
+            }else{
+                nums3[ptr]=nums2[j];
+                j++;
+                ptr++;
+            }
         }
-        for(int i=0; i<m; i++){
-            nums3[ptr]=nums2[i];
-            ptr++;
-        }
-        Arrays.sort(nums3);
 
-        if(x%2!=0) return (double)nums3[x/2];
-        double sum=((double)nums3[x/2]+nums3[(x/2)-1])/2;
-        return sum;
+        //Remaining ones
+        while(i<n){
+            nums3[ptr]=nums1[i];
+            i++;
+            ptr++;
+        }
+        while(j<m){
+            nums3[ptr]=nums2[j];
+            j++;
+            ptr++;
+        }
+        if(x%2==1) return (double)nums3[x/2];
+        double median=((double)nums3[x/2]+nums3[(x/2)-1])/2;
+        return median;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Extreme brute force
+        // int[] nums3=new int[x];
+        // int ptr=0;
+        // for(int i=0; i<n; i++){
+        //     nums3[ptr]=nums1[i];
+        //     ptr++;
+        // }
+        // for(int i=0; i<m; i++){
+        //     nums3[ptr]=nums2[i];
+        //     ptr++;
+        // }
+        // Arrays.sort(nums3);
+
+        // if(x%2!=0) return (double)nums3[x/2];
+        // double sum=((double)nums3[x/2]+nums3[(x/2)-1])/2;
+        // return sum;
     }
 }
