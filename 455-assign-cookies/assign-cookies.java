31bf933921022a14f6f428e6e98b11c1d9e4c304
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-08-10 15:04:09 +0530

    Time: 14 ms (91.91%) | Memory: 51.5 MB (37.51%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -2,19 +2,49 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        // g matlab greed factor har ek child ka
-        int harchildkigreed=g.length; 
-        //s matlab cookies ka size
-        int cookieskasize=s.length; 
-        int left=0;
-        int right=0;
-        while(left<harchildkigreed && right<cookieskasize){
-            if(g[left]<=s[right]){
-                left++;
+        int i=0;
+        int j=0;
+        int cnt=0;
+        while(i<g.length && j<s.length){
+            if(g[i]<=s[j]){
+                cnt++;
+                i++;
+                j++;
+            }else{
+                j++;
             }
-            right++;
         }
-        return left;
+        return cnt;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Arrays.sort(g);
+        // Arrays.sort(s);
+        // // g matlab greed factor har ek child ka
+        // int harchildkigreed=g.length; 
+        // //s matlab cookies ka size
+        // int cookieskasize=s.length; 
+        // int left=0;
+        // int right=0;
+        // while(left<harchildkigreed && right<cookieskasize){
+        //     if(g[left]<=s[right]){
+        //         left++;
+        //     }
+        //     right++;
+        // }
+        // return left;
         
     }
 }
