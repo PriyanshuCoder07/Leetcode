64d8abbd22b01a1d2f2072dadc3035529798fcd8
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-08-13 02:51:15 +0530

    Time: 4 ms (23.02%) | Memory: 48.2 MB (65.51%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,15 +1,46 @@
 class Solution {
+    public boolean findTarget(int[] arr, int m, int target){
+        int low=0;
+        int high=m-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target)
+                return true;
+            else if(arr[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+    }
     public boolean searchMatrix(int[][] matrix, int target) {
         int n=matrix.length; 
         int m=matrix[0].length;
         for(int row=0; row<n; row++){
-            for(int col=0; col<m; col++){
-                if(matrix[row][col]==target){
-                    return true;
-                }
-            }
+            if(findTarget(matrix[row],m,target)) return true;
         }
         return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Brute force normally traverse all the array and having the time complexity as n*m
+        // for(int row=0; row<n; row++){
+        //     for(int col=0; col<m; col++){
+        //         if(matrix[row][col]==target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
         
     }
 }
