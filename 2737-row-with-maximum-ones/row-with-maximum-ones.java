62087082a9ff9d6f245c55ad4d6036668a10fb61
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-08-13 02:07:16 +0530

    Time: 4 ms (84.54%) | Memory: 48.2 MB (54.92%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -1,21 +1,41 @@
 class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
-        int n=mat.length; 
-        int m=mat[0].length;
+        int rows=mat.length;
+        int cols=mat[0].length;
         int ans[]=new int[2];
-        int maxcnt=0;
-        for(int i=0; i<n; i++){
+        int maxones=0;
+        for(int row=0; row<rows; row++){
             int cnt=0;
-            for(int j=0; j<m; j++){
-                if(mat[i][j]==1)cnt++;
+            for(int col=0; col<cols; col++){ 
+                if(mat[row][col]==1){
+                    cnt++;
+                }
             }
-            if(cnt>maxcnt){
-                maxcnt=cnt;
-                ans[0]=i;
+            if(cnt>maxones){
+                maxones=cnt;
+                ans[0]=row;
                 ans[1]=cnt;
             }
         }
+
         return ans;
+
+        // int n=mat.length; 
+        // int m=mat[0].length;
+        // int ans[]=new int[2];
+        // int maxcnt=0;
+        // for(int i=0; i<n; i++){
+        //     int cnt=0;
+        //     for(int j=0; j<m; j++){
+        //         if(mat[i][j]==1)cnt++;
+        //     }
+        //     if(cnt>maxcnt){
+        //         maxcnt=cnt;
+        //         ans[0]=i;
+        //         ans[1]=cnt;
+        //     }
+        // }
+        // return ans;
         
     }
 }
