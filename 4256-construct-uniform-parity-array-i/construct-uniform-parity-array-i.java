ee12e69069c3436b1640cdb93db9ef39af29d605
PriyanshuCoder07 <Kumarpriyanshu954@gmail.com>
2026-09-02 10:34:39 +0530

    Time: 1 ms (31.89%) | Memory: 45.3 MB (16.73%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int n=nums1.length;
+        int ec=0,oc=0;
+        for(int i=0; i<nums1.length; i++){
+            if(nums1[i]%2==0) ec++;
+            else oc++;
+        }
+        if(ec==n || oc==n) return true;
+        else if(ec==n/2 && oc==n/2) return true;
+        return true;
+    }
+}
